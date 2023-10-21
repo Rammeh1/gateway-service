3d5536db736c143a3398298e6552e986ae670857
“rammeh” <nasr.rammeh@gmail.com>
2023-10-22 00:06:11 +0100

    first commit

---
+++ src/main/java/org/ms/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,34 @@
+package org.ms.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc,
+			DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+	}
+
+	/*
+	 * @Bean RouteLocator routeLocator(RouteLocatorBuilder builder) { return
+	 * builder.routes().route(r ->
+	 * r.path("/clients/**").uri("http://localhost:8081/")) .route(r ->
+	 * r.path("/produits/**").uri("http://localhost:8082/")).build(); }
+	 * 
+	 * @Bean RouteLocator routeLocator(RouteLocatorBuilder builder) { return
+	 * builder.routes().route(r -> r.path("/clients/**").uri("lb://CLIENT-SERVICE"))
+	 * .route(r -> r.path("/produits/**").uri("lb://PRODUIT-SERVICE")).build(); }
+	 */
+
+}
